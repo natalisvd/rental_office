8b862b71d681b73c61cbc60c41ee25ac045aa7c5
tolyaganzin <tolyaganzin@gmail.com>
2024-11-13 11:44:53 +0200

    fe create service and add button for testing api #6

---
+++ rental-office/src/app/services/map.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  apiUrl: string = `${environment.backendUrl}/v1/`;
+
+  constructor(private http: HttpClient) { }
+
+  getMapInfo(id: number): Observable<any> {
+    const requestPayload = { id: id };
+    return this.http.post<any>(this.apiUrl + 'getmapinfo', requestPayload);
+  }
+
+}
